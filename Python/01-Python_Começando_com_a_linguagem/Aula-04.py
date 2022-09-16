39b1a4a5cfa896712065c5ddb4c6846344403708
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2022-09-16 19:29:38 -0300

    Adicionado arquivos

---
+++ Python/01-Python_Começando_com_a_linguagem/Aula-04.py
@@ -0,0 +1,45 @@
+# print normal
+# output: Brasil ganhou 5 titulos mundiais
+print("Brasil ganhou 5 titulos mundiais")
+
+# print com parametro <sep=" ">
+# output: Brasil ganhou 5 titulos mundiais
+print("Brasil ganhou 5 titulos mundiais", sep="-")
+
+# print com parametro <sep="-">
+# output: Brasil-ganhou-5-titulos mundiais
+print("Brasil", "ganhou", 5, "titulos mundiais", sep="-")
+
+# print com parametro <sep="">
+# output: Brasilganhou5titulos mundiais
+print("Brasil", "ganhou", 5, "titulos mundiais", sep="")
+
+# print com parametro <end="\n">
+# output: Brasil ganhou 5 titulos mundiais
+print("Brasil", "ganhou", 5, "titulos mundiais", end="\n")
+
+# print com parametro <end="">
+# output: Brasil ganhou 5 titulos mundiais
+print("Brasil", "ganhou", 5, "titulos mundiais", end="")
+
+# print com parametro <end="END">
+# output: Brasil ganhou 5 titulos mundiaisEND
+print("Brasil", "ganhou", 5, "titulos mundiais", end="END")
+
+# print com parametro padrão>
+# output: Brasil ganhou 5 titulos mundiais
+print("Brasil", "ganhou", 5, "titulos mundiais")
+
+print("===============================================")
+
+pais = "Italia"
+type(pais)
+quantidade = 4
+type(quantidade)
+
+print(pais, "ganhou", quantidade, "titulos mundiais")
+
+pais = "Alemanha"
+quantidade = 4
+
+print(pais, "ganhou", quantidade, "titulos mundiais")
